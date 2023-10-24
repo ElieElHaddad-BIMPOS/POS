765193e888737ddb5721e5b706c6197e439b5c3f
Elie El Haddad <elie.haddad@bimpos.com>
2023-10-24 11:05:41 +0300

    sms

--- resources/js/Pages/BackOffice/Products.vue
+++ resources/js/Pages/BackOffice/Products.vue
@@ -6,57 +6,103 @@ import NavLink from '@/Components/NavLink.vue';
 </script>
 <style>
 .product-table {
-    max-width: 800px;
-    padding: 20px;
-    background-color: #fff;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  min-width: 800px;
+  max-width: 100%;
+  padding: 10px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  overflow: auto;
+}
+.table {
+  align-content: center;
+  align-self: center;
 }
 </style>
 <template>
     <BackOfficeLayout title="Products">
 
         
-        <div class="py-12">
+        <div class="py-14 ">
             
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mx-auto sm:px-12 lg:px-16" >
+              <div class="d-flex justify-content-center">
+                <div class="product-table">
+                  <table class="table table-striped table-hover table-responsive">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">First</th>
+                      <th scope="col">Last</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
+                      <th scope="col">Handle</th>
 
-                <div class="bg-dark overflow-hidden shadow-xl sm:rounded-lg">
-                  <div class="product-table">
-                    <div class="container">
-                      <table class="table table-striped table-hover table-responsive">
-                        <thead>
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">2</th>
+                      <td>Jacob</td>
+                      <td>Thornton</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <th scope="row">3</th>
+                      <td colspan="2">Larry the Bird</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                      <td>@twitter</td>
+                    </tr>
+                  </tbody>
+                  </table>
                 </div>
+              </div>
             </div>
+
         </div>
     </BackOfficeLayout>
 </template>
